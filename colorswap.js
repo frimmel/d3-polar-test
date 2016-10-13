37942d0e31c0102cd939ed4109d5705bd3de369a
John Frimmel <jrfrimme@unca.edu>
2016-10-13 10:54:02 -0400

    changes layout

--- colorswap.js
+++ colorswap.js
@@ -79,6 +79,76 @@ function makeUpDownLineGraph (data, loc, selector) {
         width = 600 - margin.left - margin.right,
         height = 270 - margin.top - margin.bottom;
 
+    var averages = processColorData(data, loc);
+
+    // Set the ranges
+    var x = d3.scaleLinear().range([0, width])
+        .domain([0, d3.max(data, function(d) { return d.day; })]);
+    var y = d3.scaleLinear().range([height, 0])
+        .domain([0, 100]);
+
+
+    // Define the axes
+    var xAxis = d3.axisBottom(x)
+        .ticks(5);
+    
+    var yAxis = d3.axisLeft(y)
+        .ticks(6);
+
+    // Define the line
+    var valueline = d3.line()
+        .x(function(d) { return x(d.day); })
+        .y(function(d) { return y(d[loc]); });
+    
+    // Adds the svg canvas
+    var svg = d3.select(selector)
+        .append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", 
+                  "translate(" + margin.left + "," + margin.top + ")");
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+    /**
+     * This block of code draws the point at each data point
+     */
+    svg.selectAll("point")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("class", "point")
+      .attr("transform", function(d) {
+        var coors = valueline([d]).slice(1).slice(0, -1);
+        return "translate(" + coors + ")"
+      })
+      .attr("r", 3)
+      .attr("stroke", "#000")
+      .attr("fill",function(d,i){
+        return computeColor(d[loc], averages[i%46], 3);
+      });
+}
+
+function makeUpDownAverageLineGraph (data, loc, selector) {
+    // Set the dimensions of the canvas / graph
+    var margin = {top: 30, right: 20, bottom: 30, left: 50},
+        width = 600 - margin.left - margin.right,
+        height = 270 - margin.top - margin.bottom;
+
     var median = findOverallMedian(data, loc);
 
     // Set the ranges
@@ -227,7 +297,7 @@ function makeUpDownOverlapingLineGraph (data, loc, selector) {
 }
 
 function drawUpDownPolar(data, loc, selector) {
-    var width = 960,
+    var width = 500,
         height = 500,
         radius = Math.min(width, height) / 2 - 30;
 
